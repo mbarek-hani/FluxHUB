6fdc3c14ff754d96fae2593157c806e3ee73c0fe
mbarek-hani <mbarekhani30@gmail.com>
2026-05-16 16:06:37 +0100

    Create market_place.go

---
+++ routes/market_place.go
@@ -0,0 +1,15 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func setupMarketPlaceRoutes(router *gin.Engine, cfg RouterConfig) {
+	v1 := router.Group("/v1")
+	v1.GET("/public-key", cfg.DownloadCtrl.GetPublicKey)
+
+	plugins := v1.Group("/plugins")
+	plugins.POST("/submit", cfg.PluginCtrl.Submit)
+	plugins.GET("", cfg.PluginCtrl.ListApproved)
+	plugins.GET("/download/:id/:version", cfg.DownloadCtrl.Download)
+	plugins.GET("/:id/versions", cfg.DownloadCtrl.GetVersionInfo)
+	plugins.GET("/:id/scan", cfg.PluginCtrl.GetScanResult)
+}
